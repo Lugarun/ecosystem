c02e5b9ba34009ce46cfa24bac933a148ae1e885
Lukas Schmidt <lfschmidt.me@gmail.com>
2021-04-28 15:43:12 -0400

    Set up laptop autoxrandr config

--- profiles/xmonad/home.nix
+++ profiles/xmonad/home.nix
@@ -9,4 +9,31 @@
   xsession.windowManager.xmonad.enable = true;
   xsession.windowManager.xmonad.enableContribAndExtras = true;
   xsession.windowManager.xmonad.config = ./xmonad.hs;
+
+  # run autorandr -c to trigger the setting
+  programs.autorandr = {
+    enable = true;
+    profiles = {
+      "laptop-dual" = {
+        fingerprint = {
+          "HDMI-A-0" = "00ffffffffffff0004721803ccdf27342a17010380331d782a6275a3554fa027125054b30c00714f818081c081009500b300d1c00101023a801871382d40582c4500fd1e1100001e000000fd00324c1e5011000a202020202020000000ff004c58314141303033343231300a000000fc00416365722048323336484c0a20014c02031a7147010312131f1004230907078301000065030c001000023a801871382d40582c4500fd1e1100001e011d007251d01e206e285500fd1e1100001e011d00bc52d01e20b8285540fd1e1100001e8c0ad090204031200c405500fd1e110000180000000000000000000000000000000000000000000000000000000000e5";
+          "eDP" = "00ffffffffffff0006af3d4000000000211c0104a51f1178039b85925659902920505400000001010101010101010101010101010101143780b87038244010103e0035ae10000018000000000000000000000000000000000020000000fe0041554f0a202020202020202020000000fe004231343048414e30342e30200a0097";
+        };
+        config = {
+          "HDMI-A-0" = {
+            enable = true;
+            primary = true;
+            position = "0x0";
+            mode = "1920x1080";
+          };
+          "eDP" = {
+            enable = true;
+            primary = false;
+            position = "0x1080";
+            mode = "1920x1080";
+          };
+        };
+      };
+    };
+  };
 }
