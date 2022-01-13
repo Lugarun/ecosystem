0c185e4ab6ce4ffc000858f4681f8ca7b2b01405
Lukas Schmidt <lfschmidt.me@gmail.com>
2022-01-13 15:55:54 -0500

    Dejarble jupyter-python error output

--- modules/emacs/doom/config.el
+++ modules/emacs/doom/config.el
@@ -107,3 +107,14 @@
                                         (if (member (message-sendmail-envelope-from)
                                                     '("lfschmid@uwaterloo.ca" "lfschmidt@uwaterloo.ca"))
                                             'delete 'sent)))
+
+;; dejarble jupyter-python error output
+(defun ek/babel-ansi ()
+  (when-let ((beg (org-babel-where-is-src-block-result nil nil)))
+    (save-excursion
+      (goto-char beg)
+      (when (looking-at org-babel-result-regexp)
+        (let ((end (org-babel-result-end))
+              (ansi-color-context-region nil))
+          (ansi-color-apply-on-region beg end))))))
+(add-hook 'org-babel-after-execute-hook 'ek/babel-ansi)
